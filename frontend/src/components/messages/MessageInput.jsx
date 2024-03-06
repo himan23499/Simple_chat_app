85fa23698ce234da0b5246202a76196573068994
himan23499 <82254957+himan23499@users.noreply.github.com>
2024-03-06 23:34:43 +0530

    Emoji Commit2

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -6,6 +6,7 @@ import EmojiPicker from 'emoji-picker-react';
 const MessageInput = () => {
   const [message,setMessage] = useState("");
   const {loading,sendMessage} = useSendMessage();
+  const [emoji,setEmoji] = useState(false);
   const handleSubmit = async (e)=>{
     e.preventDefault();
     if(!message) return;
@@ -16,8 +17,8 @@ const MessageInput = () => {
   return (
     <form className="px-4 my-3" onSubmit={handleSubmit}>
         <div className="w-full relative">
-            <button type="submit" className="absolute inset-y-0 pr-5 end-0 flex items-center pe-3">
-            <EmojiPicker ></EmojiPicker>
+            <button type="submit" onClick={()=>setEmoji(!emoji)}className="absolute inset-y-0 pr-5 end-0 flex items-center pe-3">
+            <EmojiPicker open={emoji}></EmojiPicker>
             </button>
             <input type="text" className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white" placeholder="Send Message" value={message} onChange={(e)=>setMessage(e.target.value)}>
             </input>
