c98107b0e31050a3748b1b1ded050e117ae1ae27
himan23499 <82254957+himan23499@users.noreply.github.com>
2024-03-06 12:35:56 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,21 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import connectToMongoDB from './db/connectToMongoDb.js';
 import {app,server} from './socket/socket.js'
+import path from "path"
 dotenv.config();
 // const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json())
 app.use(cookieParser());
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messagesRoutes);    
 app.use('/api/users',userRoutes);    
-
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
 
 // app.get('/',(req,res)=>{
 //     //root route
