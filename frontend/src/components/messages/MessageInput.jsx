8bbaf6d7ea43b35df01e206998dd074a2421bf37
himan23499 <82254957+himan23499@users.noreply.github.com>
2024-03-07 07:21:41 +0530

    Emoji Commit4

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -17,8 +17,8 @@ const MessageInput = () => {
   return (
     <form className="px-4 my-3" onSubmit={handleSubmit}>
         <div className="w-full relative">
-            <button type="submit" onClick={()=>setEmoji(!emoji)}className="absolute inset-y-0 pr-5 end-0 flex items-center pe-3">
-            {emoji?<EmojiPicker open={emoji}></EmojiPicker>:<i className="fa-solid fa-plus"></i>}
+            <button type="submit" onClick={()=>setEmoji(!emoji)}className="absolute inset-y-0 mr-10 end-0 flex items-center pe-3">
+            {emoji?(<><EmojiPicker open={emoji}></EmojiPicker><i className="fa-solid fa-minus pl-4"/></>):<i className="fa-solid fa-plus"></i>}
             </button>
             <input type="text" className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white" placeholder="Send Message" value={message} onChange={(e)=>setMessage(e.target.value)}>
             </input>
