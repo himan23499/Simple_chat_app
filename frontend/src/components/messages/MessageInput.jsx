ef1e90eb55301659b4febd73123699ffe2dc02a2
himan23499 <82254957+himan23499@users.noreply.github.com>
2024-03-07 11:48:08 +0530

    Emoji Commit6

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -2,11 +2,12 @@
 import { useState } from 'react';
 import {BsSend} from 'react-icons/bs'
 import useSendMessage from '../../hooks/useSendMessage';
-import EmojiPicker from 'emoji-picker-react';
+// import EmojiPicker from 'emoji-picker-react';
+import InputEmoji from 'react-input-emoji';
 const MessageInput = () => {
   const [message,setMessage] = useState("");
   const {loading,sendMessage} = useSendMessage();
-  const [emoji,setEmoji] = useState(false);
+  // const [emoji,setEmoji] = useState(false);
   const handleSubmit = async (e)=>{
     e.preventDefault();
     if(!message) return;
@@ -17,11 +18,12 @@ const MessageInput = () => {
   return (
     <form className="px-4 my-3" onSubmit={handleSubmit}>
         <div className="w-full relative">
-            <button type="submit" onClick={()=>setEmoji(!emoji)}className="absolute inset-y-0 mr-10 end-0 flex items-center pe-3">
-            {emoji?(<><EmojiPicker open={emoji} onChange={(e)=>setMessage(e.target.value)}></EmojiPicker><i className="fa-regular fa-face-smile"></i></>):<i className="fa-regular fa-face-smile"></i>}
-            </button>
-            <input type="text" className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white" placeholder="Send Message" value={message} onChange={(e)=>setMessage(e.target.value)}>
-            </input>
+            {/* <button type="submit" onClick={()=>setEmoji(!emoji)}className="absolute inset-y-0 mr-10 end-0 flex items-center pe-3">
+            {emoji?(<><EmojiPicker open={emoji} className="p-3" onChange={(e)=>setMessage(e.target.value)}></EmojiPicker><i className="fa-regular fa-face-smile"></i></>):<i className="fa-regular fa-face-smile"></i>}
+            </button> */}
+            {/* <input type="text" className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white" placeholder="Send Message" value={message} onChange={(e)=>setMessage(e.target.value)}>
+            </input> */}
+            <InputEmoji className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white" placeholder="Send Message" value={message} onChange={(e)=>setMessage(e.target.value)}></InputEmoji>
             <button type="submit" className="absolute inset-y-0  end-0 flex items-center pe-3">
             {loading ? <div className='loading loading-spinner'></div> : <BsSend />}
             </button>
